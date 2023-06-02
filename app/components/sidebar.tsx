2143419a32213a15500e76bbea77a124f0370166
matrix1991 <31958187+matrix1991@users.noreply.github.com>
2023-06-03 00:20:28 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -100,7 +100,7 @@ export function SideBar(props: { className?: string }) {
         <div className={styles["sidebar-sub-title"]}>
          chatgpt及midjourney注册升级，仅需官方价8-9折
           AI电商主图，299元/5张，AI全案解决方案
-          咨询丸子微信：wzmz2011
+               咨询丸子微信：wzmz2011
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
